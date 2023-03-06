6da5e14f2311b34e7abe1bfb5b100ce607970739
MirrorX <88570990+mirrorx-bot@users.noreply.github.com>
2023-03-06 23:04:09 +0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,157 @@
+const axios = require("axios");
+const express = require("express");
+const cors = require("cors");
+
+const morgan = require("morgan");
+const apicache = require("apicache");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(cors());
+
+app.use(morgan('dev'));
+let cache = apicache.middleware;
+app.use(cache('1 day'))
+
+const port = process.env.PORT || 3000;
+
+app.get("/", async (req, res) => {
+    let contents = [];
+
+    axios.get("https://prod-api-cached-2.viewlift.com/content/pages?site=prothomalo&path=%2F&includeContent=true&moduleOffset=0&moduleLimit=28&languageCode=bn&countryCode=BD&userState=bG9nZ2VkT3V0").then((response) => {
+        let modules = response.data.modules;
+        modules.forEach((module) => {
+            if (module.moduleType == "CuratedTrayModule") {
+                let moduleName = module.title ? module.title : "carousel";
+                let moduleContents = [];
+                module.contentData.forEach((content) => {
+                    let contentData = {
+                        title: content.gist.title,
+                        id: content.gist.originalObjectId,
+                        permalink: content.gist.permalink,
+                        description: content.gist.description,
+                        type: content.gist.contentType,
+                        images: content.gist.imageGist,
+                    };
+                    moduleContents.push(contentData);
+                });
+                let modulesorted = {
+                    module: moduleName,
+                    contents: moduleContents,
+                };
+                contents.push(modulesorted);
+            }
+        });
+
+        res.json(contents);
+    }).catch((error) => {
+        console.log(error);
+    });
+});
+
+app.get("/movies", async (req, res) => {
+    let contents = [];
+
+    axios.get("https://prod-api-cached-2.viewlift.com/content/pages?path=%2Fmovies&site=prothomalo&includeContent=true&moduleOffset=3&moduleLimit=20&languageCode=bn&countryCode=BD&userState=bG9nZ2VkT3V0").then((response) => {
+        let modules = response.data.modules;
+        modules.forEach((module) => {
+            if (module.moduleType == "CuratedTrayModule") {
+                let moduleName = module.title ? module.title : "carousel";
+                let moduleContents = [];
+                module.contentData.forEach((content) => {
+                    let contentData = {
+                        title: content.gist.title,
+                        id: content.gist.originalObjectId,
+                        permalink: content.gist.permalink,
+                        description: content.gist.description,
+                        type: content.gist.contentType,
+                        images: content.gist.imageGist,
+                    };
+                    moduleContents.push(contentData);
+                });
+                let modulesorted = {
+                    module: moduleName,
+                    contents: moduleContents,
+                };
+                contents.push(modulesorted);
+            }
+        });
+
+        res.json(contents);
+    }).catch((error) => {
+        console.log(error);
+    });
+});
+
+app.get("/series", async (req, res) => {
+    let contents = [];
+
+    axios.get("https://prod-api-cached-2.viewlift.com/content/pages?path=%2Fseries&site=prothomalo&includeContent=true&moduleOffset=0&moduleLimit=10&languageCode=bn&countryCode=BD&userState=bG9nZ2VkT3V0").then((response) => {
+        let modules = response.data.modules;
+        modules.forEach((module) => {
+            if (module.moduleType == "CuratedTrayModule") {
+                let moduleName = module.title ? module.title : "carousel";
+                let moduleContents = [];
+                module.contentData.forEach((content) => {
+                    let contentData = {
+                        title: content.gist.title,
+                        id: content.gist.originalObjectId,
+                        permalink: content.gist.permalink,
+                        description: content.gist.description,
+                        type: content.gist.contentType,
+                        images: content.gist.imageGist,
+                    };
+                    moduleContents.push(contentData);
+                });
+                let modulesorted = {
+                    module: moduleName,
+                    contents: moduleContents,
+                };
+                contents.push(modulesorted);
+            }
+        });
+
+        res.json(contents);
+    }).catch((error) => {
+        console.log(error);
+    });
+});
+
+app.get("/shows", async (req, res) => {
+    let contents = [];
+
+    axios.get("https://prod-api-cached-2.viewlift.com/content/pages?site=prothomalo&path=%2Fshows&includeContent=true&moduleOffset=3&moduleLimit=10&languageCode=bn&countryCode=BD&userState=bG9nZ2VkT3V0").then((response) => {
+        let modules = response.data.modules;
+        modules.forEach((module) => {
+            if (module.moduleType == "CuratedTrayModule") {
+                let moduleName = module.title ? module.title : "carousel";
+                let moduleContents = [];
+                module.contentData.forEach((content) => {
+                    let contentData = {
+                        title: content.gist.title,
+                        id: content.gist.originalObjectId,
+                        permalink: content.gist.permalink,
+                        description: content.gist.description,
+                        type: content.gist.contentType,
+                        images: content.gist.imageGist,
+                    };
+                    moduleContents.push(contentData);
+                });
+                let modulesorted = {
+                    module: moduleName,
+                    contents: moduleContents,
+                };
+                contents.push(modulesorted);
+            }
+        });
+
+        res.json(contents);
+    }).catch((error) => {
+        console.log(error);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
